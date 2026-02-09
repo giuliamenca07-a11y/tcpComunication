362cb1128c9d018f6ac2537d2ed89e12a9f44960
mattmoccaldo <moccaldomatteo@gmail.com>
2026-02-09 13:31:17 +0100

    first commit

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -7,12 +7,13 @@ import java.net.Socket;
 public class MainServer {
     public static void main(String[] args) {
        System.out.println("SERVER:inizio l'esecuzione");
-        ServerSocket server = new ServerSocket(2000);
         try {
             ServerSocket server = new ServerSocket(2000);
             System.err.println("SERVER: in attesa di richieste dal client.");
             Socket clientSocket = server.accept();
             System.err.println("SERVER: il client si è connesso.");
+            clientSocket.close();
+            server.close();
         } catch (IOException e) {
             System.err.println("Porta non disponibile, " +
                     "errore nella creazione della connection socket!");
