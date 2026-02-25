fd440da4a17d9d39b35d58f5b757515988851d89
mattmoccaldo <moccaldomatteo@gmail.com>
2026-02-25 11:41:07 +0100

    ultime modifiche

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,11 +1,9 @@
 package Server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MainServer {
     public static void main(String[] args) {
@@ -14,14 +12,30 @@ public class MainServer {
             ServerSocket server = new ServerSocket(2000);
             System.out.println("SERVER: in attesa di richieste dal client.");
             Socket clientSocket = server.accept();
-            System.out.println("SERVER: il client si è connesso.");
-            InputStream inputStream = clientSocket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-            String messaggio = br.readLine();
+            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String mClient = br.readLine();
+            System.out.println("SERVER: il client si è connesso" + clientSocket);
+            OutputStream outputStream = clientSocket.getOutputStream();
+            PrintWriter pw= new PrintWriter(outputStream);
+            Scanner sc = new Scanner(System.in);
+            if(mClient.isEmpty()) {
+                clientSocket.close();
+                sc.close();
+                br.close();
+            }else {
+                System.out.println("Client: " + mClient);
+                System.out.println("Scrivi un messaggio da mandare al client: ");
+                String mServer = sc.nextLine();
+                pw.println(mServer);
+                pw.flush();
+                System.out.println("Messaggio inviato");
+            }
+            /*String messaggio = br.readLine();
             System.out.println("SERVER: il client" + clientSocket +
                     "ha scritto il messaggio" + messaggio);
             clientSocket.close();
             server.close();
+             */
         } catch (IOException e) {
             System.err.println("Porta non disponibile, " +
                     "errore nella creazione della connection socket");
